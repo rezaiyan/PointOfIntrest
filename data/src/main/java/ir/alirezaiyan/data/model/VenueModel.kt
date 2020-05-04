9431a9e4f1743c5acaad2c62da0ad3a100910385
rezaiyan <alirezaiyann@gmail.com>
2020-05-04 17:10:14 +0430

    Remove useless class

--- data/src/main/java/ir/alirezaiyan/data/model/VenueModel.kt
+++
@@ -1,7 +0,0 @@
-package ir.alirezaiyan.data.model
-
-data class VenueModel(
-    val id: String, val name: String, val rating: Float, val photo: String,
-    val formattedPhone: String?, val distance: Int, val formattedAddress: List<String>?,
-    val checkinsCount: Int, val tips: String?, val query: String?
-)
